312d5c6595a7d82c0b46af64c13f237cbcec4f48
mohammadjalizi <mjalizi1376@gmail.com>
2024-12-05 16:39:43 +0330

    deploy project

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '',
-        port: '',
-       
-      },
-    ],
-  },
-}
 
+
+
+export default nextConfig;
 
 
